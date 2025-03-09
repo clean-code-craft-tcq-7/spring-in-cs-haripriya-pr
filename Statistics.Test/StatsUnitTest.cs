fbdd6a6853afe74885aa433813a7b725b5617dc5
Sudeep Prasad <sudeep.rp@gmail.com>
2025-03-09 13:52:21 +0530

    stats expectations

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -23,23 +23,10 @@ namespace Statistics.Test
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
                 new List<___>{});
-            //All fields of computedStats (average, max, min) must be
-            //Double.NaN (not-a-number), as described in
-            //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
-        }
-        [Fact]
-        public void RaisesAlertsIfMaxIsMoreThanThreshold()
-        {
-            var emailAlert = new EmailAlert();
-            var ledAlert = new LEDAlert();
-            IAlerter[] alerters = {emailAlert, ledAlert};
-
-            const float maxThreshold = 10.2;
-            var statsAlerter = new StatsAlerter(maxThreshold, alerters);
-            statsAlerter.checkAndAlert(new List<___>{0.2, 11.9, 4.3, 8.5});
-
-            Assert.True(emailAlert.emailSent);
-            Assert.True(ledAlert.ledGlows);
+            // All fields of computedStats (average, max, min) must be
+            // Double.NaN (not-a-number), as described in
+            // https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
+            // Specify the Assert statements here
         }
     }
 }
