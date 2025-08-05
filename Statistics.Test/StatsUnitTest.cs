f2ad5de5a0517b0021fd87f664d9dbb8c5d2ce72
haripriya-pr <prharipriya1998@gmail.com>
2025-08-05 16:18:57 +0530

    Solved Testcase -1

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using Statistics;
+using System.Collections.Generic;
 
 namespace Statistics.Test
 {
@@ -11,7 +12,7 @@ namespace Statistics.Test
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{1.5, 8.9, 3.2, 4.5});
+                new List<float>{(float)1.5, (float)8.9, (float)3.2, (float)4.5 });
             float epsilon = 0.001F;
             Assert.True(Math.Abs(computedStats.average - 4.525) <= epsilon);
             Assert.True(Math.Abs(computedStats.max - 8.9) <= epsilon);
@@ -22,7 +23,7 @@ namespace Statistics.Test
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{});
+                new List<float>{});
             // All fields of computedStats (average, max, min) must be
             // Double.NaN (not-a-number), as described in
             // https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 
 namespace Statistics
 {
+    public class Stats
+    {
+        public float average { get; set; }
+        public float max { get; set; }
+        public float min { get; set; }
+    }
     public class StatsComputer
     {
         public Stats CalculateStatistics(List<float> numbers)
         {
             //Implement statistics here
+            Stats currentStats = new();
+            currentStats.average = (float)numbers.Sum() / numbers.Count();
+            currentStats.max = numbers.Max();
+            currentStats.min = numbers.Min();
+            return currentStats;
         }
     }
 }
